e5f9a83162394468872816cbbeab77a45969dedc
Nuwan <nuwan.pradeep.dev@gmail.com>
2024-08-11 14:03:13 +0530

    Add project files.

---
+++ ValidationExample/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidationExample.Models;
+
+namespace ValidationExample.Controllers;
+
+public class UserController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SubmitForm(UserViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("Success");
+        }
+        return View(model);
+    }
+    public IActionResult Success()
+    {
+        return View();
+    }
+}
---
+++ ValidationExample/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationExample.Models;
+
+public class UserViewModel
+{
+    [Required(ErrorMessage = "First Name is required")]
+    [StringLength(50, ErrorMessage = "First Name can't be longer than 50 characters")]
+    public string FirstName { get; set; }
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
+    public string Email { get; set; }
+}
+
